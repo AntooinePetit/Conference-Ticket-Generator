41e7fff27c13a947d7778538b27309ac8f5f2b00
unknown <antooine.petit@gmail.com>
2025-03-05 10:26:04 +0100

    Finished: Responsive
    - Finished full responsive for the form and the generated ticket before beginning JS

--- js/script.js
+++ js/script.js
@@ -8,3 +8,34 @@
 
 //       Coding Conf
 //       Jan 31, 2025 / Austin, TX
+
+/*
+<div id="div-ticket" class="container-desktop">
+   <h1>Congrats, <span class="user-info">Full Name</span>! Your ticket is ready.</h1>
+
+   <p id="ticket-email">We've emailed your ticket to <span class="user-info">Email@Address.com</span> and will send updates in the run up to the event.</p>
+
+   <div id="ticket">
+      <div>
+      <div id="ticket-top">
+         <img src="/assets/images/logo-mark.svg" alt="Logo de Coding Conf">
+         <span>
+            <h2>Coding Conf</h2>
+            <p>Jan 31, 2025 / Austin, TX</p>
+         </span>
+      </div>
+      
+      <div id="ticket-bot">
+         <img src="/assets/images/image-avatar.jpg" alt="Photo de l'utilisateur" id="profile-picture">
+         <span>
+            <h3>Jonatant Kristof</h3>
+            <p><img src="/assets/images/icon-github.svg" alt="Icone github">Identifiant github</p>
+         </span>
+      </div>
+      </div>
+      <p id="tag">#28401</p>
+   </div>
+
+</div>
+      
+*/
