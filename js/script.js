d7aff9b8a207b1a6782cc4d426b63e35e8f1f34c
unknown <antooine.petit@gmail.com>
2025-03-05 16:50:15 +0100

    Done: default JS and fixed CSS
    - Added the default JS to generate the ticket with basic informations
    - Fixed the upload image hover in CSS
    - Still need to recover user photo and display it in the form and on the ticket using JS

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// <!-- Form ends -->
-
-//       <!-- Generated tickets starts -->
-
-//       Congrats, <!-- Full Name -->! Your ticket is ready.
-
-//       We've emailed your ticket to <!-- Email Address --> and will send updates in the run up to the event.
-
-//       Coding Conf
-//       Jan 31, 2025 / Austin, TX
-
 /*
 <div id="div-ticket" class="container-desktop">
    <h1>Congrats, <span class="user-info">Full Name</span>! Your ticket is ready.</h1>
@@ -37,5 +26,56 @@
    </div>
 
 </div>
+<div class="attribution">
+   Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+   Coded by <a href="#">Antoine Petit</a>.
+</div>
       
 */
+
+// Modification du HTML pour générer le ticket à la validation du formulaire
+
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+   e.preventDefault()
+   const profilePicture = document.querySelector('#avatar').value
+   const fullName = document.querySelector('#name').value
+   const email = document.querySelector('#email').value
+   const githubUsername = document.querySelector('#github-username').value
+   const main = document.querySelector('main')
+   const numeroTicket = Math.floor(Math.random()*99998+1)
+   main.innerHTML = `
+   <img src="assets/images/logo-full.svg" alt="Logo Coding Conf" id="logo">
+   <div id="div-ticket" class="container-desktop">
+      <h1>Congrats, <span class="user-info">${fullName}</span>! Your ticket is ready.</h1>
+
+      <p id="ticket-email">We've emailed your ticket to <span class="user-info">${email}</span> and will send updates in the run up to the event.</p>
+
+      <div id="ticket">
+         <div>
+         <div id="ticket-top">
+            <img src="/assets/images/logo-mark.svg" alt="Logo de Coding Conf">
+            <span>
+               <h2>Coding Conf</h2>
+               <p>Jan 31, 2025 / Austin, TX</p>
+            </span>
+         </div>
+         
+         <div id="ticket-bot">
+            <img src="/assets/images/image-avatar.jpg" alt="Photo de l'utilisateur" id="profile-picture">
+            <span>
+               <h3>${fullName}</h3>
+               <p><img src="/assets/images/icon-github.svg" alt="Icone github">${githubUsername}</p>
+            </span>
+         </div>
+         </div>
+         <p id="tag">#${numeroTicket}</p>
+      </div>
+
+   </div>
+   <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+      Coded by <a href="#">Antoine Petit</a>.
+   </div>
+   `
+})
